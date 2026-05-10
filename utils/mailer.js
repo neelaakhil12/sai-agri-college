e33fb982553066d75f3252b1e7eea61f624eb752
neelaakhil12 <neelaakhil12@users.noreply.github.com>
2026-05-10 16:08:39 +0530

    Fix: Change auth key from 'password' to 'pass' in mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
     secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.SMTP_USER,
-        password: process.env.SMTP_PASS,
+        pass: process.env.SMTP_PASS,
     },
 });
 
